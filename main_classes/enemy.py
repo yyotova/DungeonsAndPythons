461ef7c7edcd79265b59222c76fa3ee72b591d88
Yoanna <yoansiyotova@gmail.com>
2020-04-02 10:23:24 +0300

    Add enemy and hero

--- main_classes/enemy.py
+++ main_classes/enemy.py
@@ -1,1 +1,52 @@
+from spell import Spell
+from weapon import Weapon
 
+class Enemy(Weapon,Spell):
+    def __init__(self, health, mana, damage):
+        self.health = health
+        self.mana = mana
+        self.damage = damage
+        self.weapon = None
+        self.spell = None
+
+    def get_health(self):
+        return self.health
+
+    def get_mana(self):
+        return self.mana
+
+    def is_alive(self):
+        return self.health > 0
+
+    def can_cast(self):
+        return self.mana > 0
+
+    def take_damage(self, damage_points):
+        self.health -= damage_points
+        if self.health < 0:
+            self.health = 0
+
+    def take_healing(self, healing_points):
+        if not self.is_alive():
+            return False
+        elif self.health == self.max_health:
+            return False
+        else:
+            self.health += healing_points
+            return True
+
+    def take_mana(self, mana_points):
+        if not self.mana == self.max_mana:
+            self.mana += mana_points
+
+    def attack(self, by):
+        if by == 'weapon':
+            if self.weapon != None:
+                return self.weapon.damage
+            else:
+                return 0
+        else:
+            if self.spell != None:
+                return self.spell.damage
+            else:
+                return 0
