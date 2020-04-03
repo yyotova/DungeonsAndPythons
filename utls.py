985ad502f3d4eb2ba5b3164b35134436c414e192
Yoanna <yoansiyotova@gmail.com>
2020-04-03 13:33:01 +0300

    Add converting enemies into instances

--- utls.py
+++ utls.py
@@ -1,25 +1,33 @@
 from main_classes.potion import Potion
 from main_classes.spell import Spell
 from main_classes.weapon import Weapon
+from main_classes.enemy import Enemy
 
-def convert_into_treasures_as_instance(dict_with_treasures):
+def convert_treasures_as_instance(dict_with_treasures):
     list_with_instance = []
 
     for key, value in dict_with_treasures.items():
         if key == 'potion':
             for potion in value:
                 if 'health' in potion:
-                    list_with_instance.append(Potion('health', potion['health']))
+                    list_with_instance.append(Potion(potion = 'health', points = potion['health']))
                 elif 'mana' in potion:
-                    list_with_instance.append(Potion('mana', potion['mana']))
+                    list_with_instance.append(Potion(potion = 'mana', points = potion['mana']))
 
         elif key == 'spell':
             for spell in value:
-                list_with_instance.append(Spell(spell['name'], spell['damage'], spell['mana_cost'], spell['cast_range']))
+                list_with_instance.append(Spell(name = spell['name'],damage = spell['damage'],mana_cost = spell['mana_cost'],cast_range = spell['cast_range']))
 
         elif key == 'weapon':
             for weapon in value:
-                list_with_instance.append(Weapon(weapon['name'], weapon['damage']))
+                list_with_instance.append(Weapon(name = weapon['name'],damage = weapon['damage']))
 
+    return list_with_instance
+
+def convert_enemies_as_instance(dict_with_enemies):
+    list_with_instance = []
+
+    for enemy in dict_with_enemies['enemy']:
+        list_with_instance.append(Enemy(health = enemy['health'],damage = enemy['damage'], mana = enemy['mana']))
 
     return list_with_instance
