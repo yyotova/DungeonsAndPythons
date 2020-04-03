98cb469124db72e40be35bad965a17d77e264d39
Yoanna <yoansiyotova@gmail.com>
2020-04-03 12:03:50 +0300

    Add reading from file for treasures and enemies

--- file_manipulation/read_file.py
+++ file_manipulation/read_file.py
@@ -1,22 +1,49 @@
 from copy import deepcopy 
-def read_from_file(file_name):
-	level_array=[]
-	treasures_list=[]
-	file=open(file_name,"r")
-	content=file.readlines()[:-1]
-	for line in content:
-		helper_list=[]
-		for letter in range(0,len(line)):
-			if line[letter]!="\n":
-				helper_list.append(line[letter])
-		level_array.append(helper_list)
-	file.close()
-	file=open(file_name,"r")
-	treasure=file.readlines()[-1:]
-	treasure=treasure[0][:-2]
-	treasure=treasure[1::]
-	# print(treasure)
-	treasures_list=treasure.split(",")
+import json
 
-	file.close()
-	return level_array,treasures_list
+def read_from_file_with_first_level(file_name):
+    level_array = []
+    
+    file = open(file_name,"r")
+    content = file.readlines()
+    for line in content:
+        helper_list=[]
+        for letter in range(0,len(line)):
+            if line[letter] != "\n":
+                helper_list.append(line[letter])
+        level_array.append(helper_list)
+
+    file.close()
+
+    return level_array
+
+def read_from_file_with_treasures(file_name):
+    treasures = {
+    'potion': [],
+    'spell': [],
+    'weapon': []
+    }
+    with open(file_name) as f:
+        for line in f:
+            treasure, description = line.strip().split(None, 1)
+
+            treasures[treasure].append(description.strip())
+
+    #converting to a dictionary
+    for key, value in treasures.items():
+        for index in range(len(value)):
+            value[index] = json.loads(value[index])
+
+    return treasures
+
+def read_from_file_with_enemies(file_name):
+    enemies = {
+    'enemy': []
+    }
+
+    with open(file_name) as f:
+        for line in f:
+            enemies['enemy'].append(json.loads(line))
+
+    return enemies
+  
--- file_manipulation/test_read_file.py
+++ file_manipulation/test_read_file.py
@@ -1,17 +1,51 @@
 import unittest
-from read_file import read_from_file
+from read_file import (read_from_file_with_first_level,
+                    read_from_file_with_treasures,
+                    read_from_file_with_enemies)
+
 class test_read_from_file(unittest.TestCase):
-	def test_read_from_file_func(self):
-		res, tre=read_from_file("level1.txt")
-		exp=[
-		["S",".","#","#",".",".",".",".",".","T" ],
-		["#","T","#","#",".",".","#","#","#","."],
-		["#",".","#","#","#","E","#","#","#","E"],
-		["#",".","E",".",".",".","#","#","#","."],
-		["#","#","#","T","#","#","#","#","#","G"]
-		]
-		self.assertEqual(res,exp)
-		self.assertEqual(tre, ["health","mana","weapon"])
+
+    def test_read_from_file_with_first_level_returns_list_with_each_line_as_list_of_strings(self):
+        res = read_from_file_with_first_level("level1.txt")
+        exp=[
+        ["S",".","#","#",".",".",".",".",".","T" ],
+        ["#","T","#","#",".",".","#","#","#","."],
+        ["#",".","#","#","#","E","#","#","#","E"],
+        ["#",".","E",".",".",".","#","#","#","."],
+        ["#","#","#","T","#","#","#","#","#","G"]
+        ]
+        
+        self.assertEqual(res,exp)
+
+    def test_read_from_file_with_treasures_returns_dic_with_each_treasure_as_dict(self):
+        res = read_from_file_with_treasures('treasures.txt')
+        exp = {
+        'potion': [{'health': 20}, {'mana': 30}], 
+        'spell': [
+            {'name': 'Pfu', 'damage': 20, 'mana_cost': 20, 'cast_range': 2},
+            {"name": "Abrakadabra","damage": 28,"mana_cost": 29,"cast_range": 2}
+            ], 
+        'weapon': [
+            {'name': 'Axe', 'damage': 30},
+            {"name": "Axe2","damage": 10.5}
+            ]
+        }
+        self.assertEqual(res,exp)
+
+
+    def test_read_from_file_with_enemies_returns_dict_with_each_enemy_as_dict(self):
+        res = read_from_file_with_enemies('enemies.txt')
+        exp = {
+        'enemy': [
+            {"name": "Targaryen","damage": 100, "mana_cost": 100,"cast_range": 2},
+            {"name":"Lannister", "damage": 100,"mana_cost": 100,"cast_range": 2},
+            {"name":"Stark", "damage": 100, "mana_cost":100,"cast_range": 2},
+            {"name":"Baratheon", "damage": 100,"mana_cost": 100, "cast_range":2}
+            ]
+        }
+
+        self.assertEqual(res, exp)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
