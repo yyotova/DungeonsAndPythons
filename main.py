1da44c8bab1b66cfb703fb5fd53ac954d6793a99
Yoanna <yoansiyotova@gmail.com>
2020-04-05 21:27:10 +0300

    Change the colors

--- main.py
+++ main.py
@@ -27,10 +27,12 @@ def message_display(text):
 
 
 map = Dungeon("level1.txt")
+
 h = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=2)
 w = Weapon(name="The Axe of Destiny", damage=20)
-h.equip(w)
 s = Spell(name="Fireball", damage=30, mana_cost=50, cast_range=200)
+
+h.equip(w)
 h.learn(s)
 # print(map.map[0][1])
 pygame.init()
@@ -38,10 +40,10 @@ white_hero = (255, 255, 255)
 
 enemy_img = pygame.image.load('enemy.png')
 hero_img = pygame.image.load("hero.jpg")
-treasure_img=pygame.image.load("treasure.png")
+treasure_img = pygame.image.load("treasure.png")
 # black_enemy = (0, 0, 0)
 red_treasure = (255, 0, 0)
-gray_wall = (138, 138, 92)
+gray_wall = (128, 64, 0)
 dis = pygame.display.set_mode((1000, 500))
 pygame.display.set_caption('Dungeons And Pythons')
 
@@ -80,21 +82,26 @@ while len(map.spawn_locations) > 0:
 
         hero_x = map.hero.location[0]
         hero_y = map.hero.location[1]
-        dis.fill((0, 102, 0))
+        dis.fill((120, 120, 105))
+
         if map.hero.location == map.gateway_location[0]:
             game_over = True
+
         if not map.hero.is_alive():
             message_display("GAME OVER")
+
         for wall in map.obstacle_localtions:
             pygame.draw.rect(dis, gray_wall, [wall[1], wall[0], 100, 100])
 
         for treasure in map.treasure_locations:
             # pygame.draw.rect(dis, red_treasure, [treasure[1], treasure[0], 100, 100])
-            dis.blit(treasure_img,(treasure[1], treasure[0]))
+            dis.blit(treasure_img, (treasure[1], treasure[0]))
+
         for enemy in map.enemy_locations:
             # pygame.draw.rect(dis, black_enemy, [enemy[1], enemy[0], 100, 100])
-            dis.blit(enemy_img,(enemy[1],enemy[0]))
-        pygame.draw.rect(dis, (139, 69, 19), [map.gateway_location[0][1], map.gateway_location[0][0], 100, 100])
+            dis.blit(enemy_img, (enemy[1], enemy[0]))
+
+        pygame.draw.rect(dis, (255, 179, 102), [map.gateway_location[0][1], map.gateway_location[0][0], 100, 100])
         dis.blit(hero_img, (hero_y, hero_x))
         pygame.display.update()
 
